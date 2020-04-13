792643e0be4adc5611b203db187ba540692bd08e
Micah Feras <mferas@berkeley.edu>
2020-04-12 20:10:43 -0700

    made drastic changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,66 @@
 <template>
   <div id="app">
-    <HelloWorld/>
+    <label>
+      切换为横向
+      <input type="checkbox" v-model="landscape" value="1">
+    </label>
+    <TreeChart :json="data" :class="{landscape: landscape.length}" @click-node="clickNode" />
+    <footer class="foot">
+        <p>© 2018 - 3018 Author
+            <a href="https://refined-x.com/" target="_blank">雅X共赏</a> 
+            Github 
+            <a href="https://github.com/tower1229/Vue-Tree-Chart" target="_blank">Vue-Tree-Chart</a>
+        </p>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TreeChart from "@/components/TreeChart";
+import gridData from "./assets/data.json"
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    TreeChart
+  },
+  data() {
+    return {
+      landscape: [],
+      data: gridData
+    }
+  },
+  methods: {
+    clickNode: function(node){
+      // eslint-disable-next-line
+      console.log(node)
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+#app .avat{border-radius: 2em;border-width:2px;}
+#app .name{font-weight:700;}
+.foot {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background: #333;
+    padding: 24px;
+    overflow: hidden;
+    color: #999;
+    font-size: 14px;
+    text-align: center;
+}
+.foot a{color:#fff;margin:0 .5em}
 </style>
